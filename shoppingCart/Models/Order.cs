3b1482e389843b6128bccaf2cad39f043092dd68
Ross <ross@ross-macbook.home>
2017-11-05 13:17:32 +0000

    Adding Product

--- shoppingCart/Models/Order.cs
+++ shoppingCart/Models/Order.cs
@@ -3,8 +3,21 @@ namespace shoppingCart.Models
 {
     public class Order
     {
-        public Order()
+        public Order(
+            string code,
+            string description,
+            decimal unitPrice)
         {
+            Code = code;
+            Description = description;
+            UnitPrice = unitPrice;
         }
+
+
+        public string Code {get; set;}
+
+        public string Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
     }
 }
