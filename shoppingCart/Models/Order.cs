92919cb89c55e1a90e47367092587c5abc815b8b
Ross <ross@ross-macbook.home>
2017-11-05 14:25:43 +0000

    Adding some functionality to the order class

--- shoppingCart/Models/Order.cs
+++ shoppingCart/Models/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace shoppingCart.Models
 {
@@ -7,28 +9,88 @@ namespace shoppingCart.Models
     {
         static int previousId = 0;
 
+        private bool _orderConfirmed;
+
         public Order(Customer customer,
-                     decimal amount,
-                     IEnumerable orderLines)
+                     List<OrderLine> orderLines)
         {
             Id = previousId += 1;
             CustomerName = customer.Name;
             Date = DateTime.Now;
             Address = customer.Address;
-            Amount = amount;
             OrderLines = orderLines;
+            _orderConfirmed = false;
         }
 
-        public int Id { get; set; }
+        #region Public Attriubtes
 
-        public string CustomerName { get; set; }
+        public int Id { get; }
 
-        public DateTime Date { get; set; }
+        public string CustomerName
+        {
+            get { return CustomerName; }
+            set
+            {
+                if (!OrderConfirmed)
+                {
+                    CustomerName = value;
+                }
+            }
+        }
 
-        public string Address { get; set; }
+        public DateTime Date
+        {
+            get { return Date; }
+            set
+            {
+                if (!OrderConfirmed)
+                {
+                    Date = value;
+                }
+            }
+        }
 
-        public decimal Amount { get; set; }
-    
-        public IEnumerable OrderLines { get; set; }
+        public string Address
+        {
+            get { return Address; }
+            set
+            {
+                if (!OrderConfirmed)
+                {
+                    Address = value;
+                }
+            }
+        }
+
+        public decimal Amount { get { return CalculateTotal(); } }
+
+        public IList<OrderLine> OrderLines { get; }
+
+        public bool OrderConfirmed { get => _orderConfirmed; }
+
+        #endregion
+
+        #region Public Functions
+
+        //Effectively makes the class immutable
+        public void ConfirmOrder()
+        {
+            _orderConfirmed = true;
+        }
+
+        #endregion
+
+        #region Private Functions
+
+        private decimal CalculateTotal()
+        {
+            decimal total = 0;
+
+            OrderLines.Select(x => total += x.Cost);
+
+            return total;
+        }
+
+        #endregion
     }
 }
